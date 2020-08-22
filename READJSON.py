34d532257bc1749a091a7739bc311f8a454b1c4a
JonuGonzalez <63934892+JonuGonzalez@users.noreply.github.com>
2020-08-21 21:40:06 -0600

    correcion json

--- READJSON.py
+++ READJSON.py
@@ -1,13 +1,13 @@
 import json
 
-def readjson():
-    file = readjson("Archivos/filejson.json","r","error")
+def leerjson():
+    file = leerjson("filejson.json",)
     data = json.load(file)
     file.close()
     print(data)
     return data
 
-dict = readjson()
+dict = leerjson()
 for element in dict:
     print(element)
 
